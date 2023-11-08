11d745a79386eb59e7f5002b19d843029c9ce917
weilu <weilu>
2023-11-08 22:10:49 +0000

    Simplify moz gold table read

--- cross_country_aggregate_dlt.py
+++ cross_country_aggregate_dlt.py
@@ -6,9 +6,7 @@ from pyspark.sql.window import Window
 # TODO: stack all country's microdata into 'boost_gold'
 @dlt.table(name=f'boost_gold')
 def boost_gold():
-    return (spark.table('boost_intermediate.moz_boost_gold')
-        .select('*')
-    )
+    return spark.table('boost_intermediate.moz_boost_gold')
 
 @dlt.table(name=f'expenditure_by_country_year')
 def expenditure_by_country_year():
@@ -16,4 +14,3 @@ def expenditure_by_country_year():
     return (dlt.read(f'boost_gold')
         .withColumn('Expenditure', sum(col("executed")).over(country_year_part))
     )
-
