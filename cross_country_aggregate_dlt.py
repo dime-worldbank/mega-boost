26684e756ffe92e38468a4d00a88a0ebb8e5f8ec
weilu <weilu>
2023-12-18 22:04:20 +0000

    Add Colombia to agg tables

--- cross_country_aggregate_dlt.py
+++ cross_country_aggregate_dlt.py
@@ -10,7 +10,8 @@ def boost_gold():
     pry = spark.table('boost_intermediate.pry_boost_gold')
     ken = spark.table('boost_intermediate.ken_boost_gold')
     pak = spark.table('boost_intermediate.pak_boost_gold')
-    return moz.union(pry).union(ken).union(pak)
+    col = spark.table('boost_intermediate.col_boost_gold')
+    return moz.union(pry).union(ken).union(pak).union(col)
 
 @dlt.table(name=f'cpi_factor')
 def cpi_factor():
