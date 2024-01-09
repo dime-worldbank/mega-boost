7f7073d6999a7fe8d314343ebcdc83c207e4af40
bhupatiraju <bhupatiraju>
2024-01-09 14:15:58 +0000

    Modified Paraguay microdata extraction
    Changed Paraguay extract microdata to read as dataframe in pandas instead of CSV writer

--- Paraguay/PRY_extract_microdata.py
+++ Paraguay/PRY_extract_microdata.py
@@ -15,23 +15,25 @@ filename = input_excel_filename(COUNTRY)
 
 # TODO: Figure out the difference between cen and Municipalidades
 disaggregated_data_sheets = ['cen']
+
 for sheet in tqdm(disaggregated_data_sheets):
     csv_file_path = f'{microdata_csv_dir}/{sheet}.csv'
-    # Open the Excel file with openpyxl
-    wb = openpyxl.load_workbook(filename, read_only=True)
-    sheet_data = wb[sheet]
+    df = pd.read_excel(filename, sheet_name=sheet, header=0)
 
-    # Write the data to a CSV file
-    with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
-        csv_writer = csv.writer(csvfile)
-
-        # Write header row
-        full_header_row = [cell.value for cell in next(sheet_data.iter_rows(max_row=1))]
-        mask = [is_named_column(cell) for cell in next(sheet_data.iter_rows(max_row=1))]
-        header_row = [x for x, include in zip(full_header_row, mask) if include]
-        csv_writer.writerow(header_row)
+    # Handle unnamed or null named columns
+    header = [col_name for col_name in df.columns if is_named_column(col_name)]
+    df = df[header]
+    df.columns = [col.strip() for col in header]
         
-        # Write data rows
-        for row in sheet_data.iter_rows(min_row=2):
-            normalized_row = [normalize_cell(cell.value) for i, cell in enumerate(row) if mask[i]]
-            csv_writer.writerow(normalized_row)
+    # Normalize cells
+    df = df.applymap(normalize_cell)
+
+    # Remove rows where all values are null
+    df = df.dropna(how='all')
+    
+    # Write to CSV
+    df.to_csv(csv_file_path, index=False, encoding='utf-8')
+
+# COMMAND ----------
+
+
