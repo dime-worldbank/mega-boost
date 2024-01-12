b032bd3e4635f6925cb559986c8885cd1b10fa62
bhupatiraju <bhupatiraju>
2024-01-12 19:03:09 +0000

    Added functional categorization. Edited country_name to match the standardized name from indicator.countries

--- Congo_DR/COD_transform_load_dlt.py
+++ Congo_DR/COD_transform_load_dlt.py
@@ -6,8 +6,7 @@ from pyspark.sql.functions import substring, col, lit, when, element_at, split, 
 TOP_DIR = "/mnt/DAP/data/BOOSTProcessed"
 INPUT_DIR = f"{TOP_DIR}/Documents/input/Countries"
 WORKSPACE_DIR = f"{TOP_DIR}/Workspace"
-COUNTRY = 'Congo'
-COUNTRY_MICRODATA_DIR = f'{WORKSPACE_DIR}/microdata_csv/{COUNTRY}'
+COUNTRY_MICRODATA_DIR = f'{WORKSPACE_DIR}/microdata_csv/Congo'
 
 CSV_READ_OPTIONS = {
     "header": "true",
@@ -40,14 +39,26 @@ def boost_silver():
                 lower(trim(regexp_replace(col("Province"), "\d+", "")))
             )
             .otherwise('Other') 
-        )
+        ).withColumn(
+        'func',
+        when(col('Grande_Fonction').startswith('01'), 'General public services')
+        .when(col('Grande_Fonction').startswith('02'), 'Defence')
+        .when(col('Grande_Fonction').startswith('03'), 'Public order and safety')
+        .when(col('Grande_Fonction').startswith('04'), 'Economic affairs')
+        .when(col('Grande_Fonction').startswith('05'), 'Environmental protection')
+        .when(col('Grande_Fonction').startswith('06'), 'Housing and community amenities')
+        .when(col('Grande_Fonction').startswith('07'), 'Health')
+        .when(col('Grande_Fonction').startswith('08'), 'Recreation, culture and religion')              
+        .when(col('Grande_Fonction').startswith('09'), 'Education')        
+        .when(col('Grande_Fonction').startswith('10'), 'Social protection')    
+    ).withColumn('is_transfer', lit(False))
     )
 
 @dlt.table(name=f'cod_boost_gold')
 def boost_gold():
     return (dlt.read(f'cod_boost_silver')
             .filter(~(col('Titre') == "1 DETTE PUBLIQUE EN CAPITAL"))
-            .withColumn('country_name', lit(COUNTRY))
+            .withColumn('country_name', lit('Congo, Dem. Rep.')) 
             .select('country_name',
                     'adm1_name',
                     col('Exercice').alias('year'),
@@ -55,5 +66,7 @@ def boost_gold():
                      # Its unclear from the notes how Montant Vote, Montant Dotation, Montant ENgage, Montant Liquide and Montant ODFC relate to approved and committed
                     col('Montant_Engagé').alias('revised'),
                     col('Montant_Payé').alias('executed'),
+                    'is_transfer',
+                    'func'
                     )
     )
