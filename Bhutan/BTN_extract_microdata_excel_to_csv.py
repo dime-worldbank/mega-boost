9c99f808e66ad736afb947414b20504fca510bb2
bhupatiraju <bhupatiraju>
2024-02-18 19:53:08 +0000

    Bhutan microdata extraction to CSV

---
+++ Bhutan/BTN_extract_microdata_excel_to_csv.py
@@ -0,0 +1,31 @@
+# Databricks notebook source
+# MAGIC %run ../utils
+
+# COMMAND ----------
+
+from tqdm import tqdm
+import openpyxl
+import pandas as pd
+
+COUNTRY = 'Bhutan'
+microdata_csv_dir = prepare_microdata_csv_dir(COUNTRY)
+filename = input_excel_filename(COUNTRY)
+
+disaggregated_data_sheets = ['BOOST']
+for sheet in tqdm(disaggregated_data_sheets):
+    csv_file_path = f'{microdata_csv_dir}/{sheet}.csv'
+    df = pd.read_excel(filename, sheet_name=sheet, header=0)
+
+    # Handle unnamed or null named columns
+    header = [col_name for col_name in df.columns if is_named_column(col_name)]
+    df = df[header]
+    df.columns = [col.strip() for col in header]
+        
+    # Normalize cells
+    df = df.applymap(normalize_cell)
+
+    # Remove rows where all values are null
+    df = df.dropna(how='all')
+    
+    # Write to CSV
+    df.to_csv(csv_file_path, index=False, encoding='utf-8')
