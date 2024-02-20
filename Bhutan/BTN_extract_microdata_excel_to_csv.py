8f17bc8d09b34dc2304eac032d65cf66dc4f7eae
bhupatiraju <bhupatiraju>
2024-02-20 20:26:41 +0000

    Added adm1_name matching to subnational population to the extraction step

--- Bhutan/BTN_extract_microdata_excel_to_csv.py
+++ Bhutan/BTN_extract_microdata_excel_to_csv.py
@@ -7,6 +7,21 @@ from tqdm import tqdm
 import openpyxl
 import pandas as pd
 
+def correct_adm1_name(name):
+    name_change = {
+        'PEMA GATSHEL': 'Pemagatshel',
+        'TASHI YANGTSE': 'Trashiyangtse',
+        'TASHIGANG': 'Trashigang',
+        'THIMPHU':'Thimphu',
+        'WANGDUE PHODANG':'Wangduephodrang'
+        }
+    if type(name)!=str:
+        return
+    for k,v in name_change.items():
+        if k in name:
+            name = name.replace(k,v)
+    return name.replace('DZONGKHAG', '').strip().title()
+    
 COUNTRY = 'Bhutan'
 microdata_csv_dir = prepare_microdata_csv_dir(COUNTRY)
 filename = input_excel_filename(COUNTRY)
@@ -26,6 +41,9 @@ for sheet in tqdm(disaggregated_data_sheets):
 
     # Remove rows where all values are null
     df = df.dropna(how='all')
+
+    # match names to the region names in subnational population
+    df['Admin2'] = df.Admin2.map(lambda x: correct_adm1_name(x) if str(x)[0]=='4' else x)    
     
     # Write to CSV
     df.to_csv(csv_file_path, index=False, encoding='utf-8')
