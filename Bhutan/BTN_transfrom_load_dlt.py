56549141c45a0edbd069af65c67de8c379a24045
bhupatiraju <bhupatiraju>
2024-03-11 12:55:45 +0000

    Added admin1 to btn_boost_gold

--- Bhutan/BTN_transfrom_load_dlt.py
+++ Bhutan/BTN_transfrom_load_dlt.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 import dlt
-from pyspark.sql.functions import substring, col, lit, when, element_at, split, upper, trim, lower, regexp_replace, regexp_extract, substring
+from pyspark.sql.functions import substring, col, lit, when, element_at, split, upper, trim, lower, regexp_replace, regexp_extract, substring, expr
 
 # Note DLT requires the path to not start with /dbfs
 TOP_DIR = "/mnt/DAP/data/BOOSTProcessed"
@@ -42,6 +42,8 @@ def boost_silver():
                 # imputing that the rest belongs to central scope (various ministries etc)
                 .otherwise("Central Scope")
             ).withColumn(
+                'admin1', trim(regexp_replace(col("ADMIN2"), '^[0-9\\s]*', ''))
+            ).withColumn(
                 'func_sub',
                 when(col('prog1').startswith('5 '), 'judiciary')
                 .when(col('prog1').startswith('30'), 'public safety')
@@ -52,7 +54,8 @@ def boost_silver():
                 # No breakdown in health expenditure into primary, secondary and tertiary       
             ).withColumn(
                 'func',
-                when(col("func_sub").isin("judiciary", "public safety") , "Public order and safety")
+                when(((col('Econ3')=='Social benefits') | col('econ4').startswith('25.01')), 'Social protection')
+                .when(col("func_sub").isin("judiciary", "public safety") , "Public order and safety")
                 # No classification into defence
                 .when((
                     col('prog1').startswith('16') |
@@ -67,7 +70,6 @@ def boost_silver():
                     col('prog1').startswith('68') |
                     col('prog1').startswith('69') |
                     col('prog1').startswith('80')), 'Health')
-                .when(((col('Econ3')=='Social benefits') | col('econ4').startswith('25.01')), 'Social protection')
                 .when(col('prog1').startswith('4 ') | col('prog1').startswith('33') | col('prog1').startswith('73'), 'Recreation, culture and religion')
                 .when(col('prog1').startswith('10'), 'Environmental protection') # 2021 and 2020 figures don't match
                 .when(
@@ -76,14 +78,15 @@ def boost_silver():
                     ) | (
                         (col('prog1').startswith('56') | col('prog1').startswith('91') | col('prog1').startswith('98')) & (col('Water&SanitationTag') == True)
                     )) ,'Housing and community amenities')
+
                 # No defence spending information
                 .when((
                     (col('prog1').startswith('53') | col('prog1').startswith('26') | col('prog1').startswith('50') | col('prog1').startswith('51')) |
                     ((col('airport')==1) & (~col('prog1').startswith('33')) & (~col('prog1').startswith('55'))) |
-                    (col('Roads')==1) |
+                    (col('Roads')==True) |
                     (col('prog1').startswith('61') | col('prog1').startswith('65') | col('prog1').startswith('66')) |
                     (col('prog1').startswith('43') | col('prog1').startswith('44') | col('prog1').startswith('45') | col('prog1').startswith('46') | col('prog1').startswith('48') | col ('prog1').startswith('83')) |
-                    (col('activity').startswith('26') | col('prog1').startswith('88') | col('prog1').startswith('89') | col('prog1').startswith('90'))                
+                    (col('activity').startswith('26') | col('prog1').startswith('53') | col('prog1').startswith('88') | col('prog1').startswith('89') | col('prog1').startswith('90'))                
                 ), 'Economic affairs')
                 .otherwise('General public services')
             )
@@ -94,13 +97,13 @@ def boost_silver():
 def boost_gold():
     return (dlt.read(f'btn_boost_silver')
             .withColumn('country_name', lit('Bhutan')) 
-            .withColumn('revised', lit(None))
             .select('country_name',
                     'adm1_name',
                     'year',
                     col('Budget').alias('approved'),
-                    'revised',
                     col('Executed').alias('executed'),
+                    expr("CAST(NULL AS DOUBLE) as revised"),
+                    col('admin1'),
                     'is_transfer',
                     'func'
             )
