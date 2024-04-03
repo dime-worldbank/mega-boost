a77014410013416977e75be97fbc942a16f4af42
nihaas21 <nihaas21>
2024-04-03 14:24:19 +0000

    Updated Extraction - Albani
    > Fixing constant's case
    > Removing loop
    > Fixing error message

--- Albania/ALB_extract_microdata_excel_to_csv.py
+++ Albania/ALB_extract_microdata_excel_to_csv.py
@@ -3,28 +3,24 @@
 
 # COMMAND ----------
 
-from tqdm import tqdm
-import openpyxl
 import pandas as pd
+MIN_NUM_OF_ROWS = 829223
+YEAR_RANGE = (2010, 2022)
 
 COUNTRY = 'Albania'
 microdata_csv_dir = prepare_microdata_csv_dir(COUNTRY)
 filename = input_excel_filename(COUNTRY)
-disaggregated_data_sheets = ['Data_Expenditures']
+sheet_name = 'Data_Expenditures'
+csv_file_path = f'{microdata_csv_dir}/{sheet_name}.csv'
 
-for sheet in tqdm(disaggregated_data_sheets):
-    csv_file_path = f'{microdata_csv_dir}/{sheet}.csv'
-    df = pd.read_excel(filename, sheet_name=sheet)
-    # Handle null and unnamed columns
-    header = [col_name for col_name in df.columns if normalize_cell(is_named_column(col_name))]
-    df = df[header]
-    df = df.dropna(how='all')
-    df = df.applymap(normalize_cell)
-    df.to_csv(csv_file_path, index=False, encoding='utf-8')
+df = pd.read_excel(filename, sheet_name=sheet_name)
 
-    min_num_of_rows = 829223
-    year_range = (2010,2022)
+# Handle null and unnamed columns
+header = [col_name for col_name in df.columns if normalize_cell(is_named_column(col_name))]
+df = df[header]
+df = df.dropna(how='all')
+df = df.applymap(normalize_cell)
+df.to_csv(csv_file_path, index=False, encoding='utf-8')
 
-    assert len(df) >= min_num_of_rows, f"Number of rows in sheet are less than 829223."
-    assert df['year'].between(*year_range).all(), f"Values in 'Year' column in sheet are not within the range {year_range}."
-
+assert len(df) >= MIN_NUM_OF_ROWS, f"Number of rows in sheet are less than expected {MIN_NUM_OF_ROWS}, got {df.shape[0]}."
+assert df['year'].between(*YEAR_RANGE).all(), f"Values in 'Year' column in sheet are not within the range {YEAR_RANGE}."
