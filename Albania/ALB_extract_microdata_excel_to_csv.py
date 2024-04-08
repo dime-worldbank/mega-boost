d4f21b70787d0d82594cecf02a77a30c2ad707fa
nihaas21 <nihaas21>
2024-04-08 15:23:14 +0000

    Year Assertion Edit
    >Fixing constants
    >Year assertion edited

--- Albania/ALB_extract_microdata_excel_to_csv.py
+++ Albania/ALB_extract_microdata_excel_to_csv.py
@@ -4,16 +4,19 @@
 # COMMAND ----------
 
 import pandas as pd
-MIN_NUM_OF_ROWS = 829223
-YEAR_RANGE = (2010, 2022)
+import numpy as np
 
+SHEET_NAME = 'Data_Expenditures'
+MIN_NUM_OF_ROWS = 829223
+START_YEAR = 2010
+END_YEAR = 2022
 COUNTRY = 'Albania'
+
 microdata_csv_dir = prepare_microdata_csv_dir(COUNTRY)
 filename = input_excel_filename(COUNTRY)
-sheet_name = 'Data_Expenditures'
-csv_file_path = f'{microdata_csv_dir}/{sheet_name}.csv'
+csv_file_path = f'{microdata_csv_dir}/{SHEET_NAME}.csv'
 
-df = pd.read_excel(filename, sheet_name=sheet_name)
+df = pd.read_excel(filename, sheet_name=SHEET_NAME)
 
 # Handle null and unnamed columns
 header = [col_name for col_name in df.columns if normalize_cell(is_named_column(col_name))]
@@ -22,5 +25,21 @@ df = df.dropna(how='all')
 df = df.applymap(normalize_cell)
 df.to_csv(csv_file_path, index=False, encoding='utf-8')
 
+existing_years = df['year'].unique()
+expected_years = np.arange(START_YEAR, END_YEAR + 1)
+
+
 assert len(df) >= MIN_NUM_OF_ROWS, f"Number of rows in sheet are less than expected {MIN_NUM_OF_ROWS}, got {df.shape[0]}."
-assert df['year'].between(*YEAR_RANGE).all(), f"Values in 'Year' column in sheet are not within the range {YEAR_RANGE}."
+def check_missing_years(expected_years, existing_years):
+    missing_year_values = []
+    for value in expected_years:
+        if value not in existing_years:
+            missing_year_values.append(value)
+
+    assert not missing_year_values, f"The following years are missing from the set: {missing_year_values}"
+
+check_missing_years(expected_years, existing_years)
+print("All expected years are present")
+
+
+
