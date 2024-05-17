13a253aba7d5ee310096b33a2fb4f2040dcb75be
bhupatiraju <bhupatiraju>
2024-05-17 17:18:18 +0000

    Added the municipalidades sheet to extraction

--- Paraguay/PRY_extract_microdata.py
+++ Paraguay/PRY_extract_microdata.py
@@ -14,7 +14,7 @@ microdata_csv_dir = prepare_microdata_csv_dir(COUNTRY)
 filename = input_excel_filename(COUNTRY)
 
 # TODO: Figure out the difference between cen and Municipalidades
-disaggregated_data_sheets = ['cen']
+disaggregated_data_sheets = ['cen', 'Municipalidades']
 
 for sheet in tqdm(disaggregated_data_sheets):
     csv_file_path = f'{microdata_csv_dir}/{sheet}.csv'
@@ -33,7 +33,3 @@ for sheet in tqdm(disaggregated_data_sheets):
     
     # Write to CSV
     df.to_csv(csv_file_path, index=False, encoding='utf-8')
-
-# COMMAND ----------
-
-
