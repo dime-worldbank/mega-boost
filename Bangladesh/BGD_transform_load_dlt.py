671bd2b763ed69a9ed263ae2327d148a2dc85240
weilu <weilu>
2024-07-24 11:52:28 +0000

    Add 2015-2018 data processing to BGD

--- Bangladesh/BGD_transform_load_dlt.py
+++ Bangladesh/BGD_transform_load_dlt.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 import dlt
 import unicodedata
-from pyspark.sql.functions import col, lower, initcap, trim, regexp_replace, when, lit, substring, expr
+from pyspark.sql.functions import col, lower, initcap, trim, regexp_extract, regexp_replace, when, lit, substring, expr
 from pyspark.sql.types import StringType
 
 
@@ -18,8 +18,8 @@ CSV_READ_OPTIONS = {
     "escape": '"',
 }
 @dlt.expect_or_drop("year_not_null", "Year IS NOT NULL")
-@dlt.table(name=f'bgd_boost_bronze')
-def boost_bronze():
+@dlt.table(name=f'bgd_2019_onward_boost_bronze')
+def boost_2019_onward_bronze():
     # TODO: expand to 2018 and before, which have diff columns
     file_paths = [f"{COUNTRY_MICRODATA_DIR}/2019.csv", f"{COUNTRY_MICRODATA_DIR}/202*.csv"]
 
@@ -32,9 +32,9 @@ def boost_bronze():
     bronze_df = bronze_df.dropna(how='all')
     return bronze_df
 
-@dlt.table(name=f'bgd_boost_silver')
-def boost_silver():
-    return (dlt.read(f'bgd_boost_bronze')
+@dlt.table(name=f'bgd_2019_onward_boost_silver')
+def boost_2019_onward_silver():
+    return (dlt.read(f'bgd_2019_onward_boost_bronze')
         .filter(col('ECON1').isin(['3 - Recurrent Expenditure', '4 - Capital Expenditure']))
         .withColumn(
             'geo1', 
@@ -118,22 +118,205 @@ def boost_silver():
         )
     )
 
-@dlt.table(name=f'bgd_boost_gold')
-def boost_gold():
-    return (dlt.read(f'bgd_boost_silver')
-            .withColumn('country_name', lit(COUNTRY))
-            .select('country_name',
-                    'year',
-                    col('BUDGET').alias('approved'),
-                    col('REVISED').alias('revised'),
-                    col('EXECUTED').alias('executed'),
-                    'admin0',
-                    'admin1',
-                    'admin2',
-                    'geo1',
-                    'func',
-                    'func_sub',
-                    'econ',
-                    'econ_sub'
+# COMMAND ----------
+
+@dlt.expect_or_drop("year_not_null", "Year IS NOT NULL")
+@dlt.table(name=f'bgd_2015_to_2018_boost_bronze')
+def boost_2015_to_2018_bronze():
+    file_paths = list(f"{COUNTRY_MICRODATA_DIR}/{year}.csv" for year in range(2015, 2019))
+
+    bronze_df = (spark.read
+                 .format("csv")
+                 .options(**CSV_READ_OPTIONS)
+                 .option("inferSchema", "true")
+                 .load(file_paths))
+    bronze_df = bronze_df.withColumn('year', col('year').cast('int'))
+    bronze_df = bronze_df.dropna(how='all')
+    return bronze_df
+
+@dlt.table(name=f'bgd_2015_to_2018_boost_silver')
+def boost_2015_to_2018_silver():
+    return (dlt.read(f'bgd_2015_to_2018_boost_bronze')
+        .filter(~col('ECON2').isin([
+            "6700-Revenue - General",
+            "7100-Investments in Shares and Equities",
+            "7300-Loans",
+            "7400-Advances to Government Employees",
+            "7500-Term Debt Repayments",
+            "7600-Floating Debt Repayments",
+            "7700-Foreign Debt Repayment",
+            "7800-Transaction with IMF",
+            "7900-Development Import Duty and VAT",
+        ]))
+        .withColumn(
+            'admin0', 
+            when(col('ADMIN0').isin(['DAO', 'UAO']), 'Regional')
+            .otherwise('Central')
+        ).withColumn(
+            'geo1', 
+            when(
+                # necessary for coercing other values to None
+                regexp_extract(col("GEO1"), r'\d', 0) != '',
+                regexp_replace(col("GEO1"), r"\d+ ", "")
+            )
+        ).withColumn(
+            'admin1',
+            when(col('admin0') == 'Central', 'Central')
+            .otherwise(col('geo1'))
+        ).withColumn(
+            'admin2',
+            col('ADMIN2')
+        ).withColumn(
+            'func',
+            # social protection needs to be first to pick up all of ECON2 6300-Pensions and Gratuities
+            when(
+                ((col('FUNC1') == 'Social Security and Welfare') | col('ECON2').startswith('6300')),
+                'Social protection'
+            ).when(
+                col('FUNC1') == 'Defence Services',
+                'Defence'
+            ).when(
+                col('FUNC1') == 'Public Order and Safety',
+                'Public order and safety'
+            ).when(
+                col('FUNC1') == 'Housing',
+                'Housing and community amenities'
+            ).when(
+                col('FUNC1') == 'Health',
+                'Health'
+            ).when(
+                col('FUNC1') == 'Recreation, Culture and Religious Affairs',
+                'Recreation, culture and religion'
+            ).when(
+                col('FUNC1') == 'Education and Technology',
+                'Education'
+            ).when(
+                # Environment needs to be before Economic affairs as it's a subcategory of FUNC1='Agriculture'
+                col('ADMIN2') == "45-Ministry of Environment, Forest and Climate Change",
+                'Environmental protection'
+            ).when(
+                col('FUNC1').isin([
+                    "Agriculture",
+                    "Energy and Power",
+                    "Transport and Communication",
+                    "Industrial and Economic Services"
+                ]),
+                'Economic affairs'
+            ).otherwise(lit('General public services'))
+        )
+        .withColumn(
+            'func_sub',
+            when(col('func') == 'Public order and safety', 
+                 when(col('ADMIN2') == '22-Public Security Division', 'public order')
+                 .otherwise('judiciary')
+            )
+            .when(col('func') == 'Education', 
+                when(
+                    (col('ADMIN2').contains('Primary Education') 
+                    | col('ADMIN2').contains('Primary and Mass Education'))
+                , 'primary education')
+                .when(
+                    (col('ADMIN2').contains('Secondary and Higher Education') 
+                    | col('ADMIN2').contains('Secondary & Higher Education'))
+                , 'secondary education')
+            )
+        ).withColumn(
+            'econ_sub',
+            when(col('ECON2').isin(["4500-Pay of Officers","4600-Pay of Establishment"]), 'basic wages')
+            .when(col('ECON2') == "4700-Allowances", 'allowances')
+            .when(
+                col('ECON2') == "4800-Supplies and Services",
+                when(
+                    (col('ECON3').startswith("4806")
+                     | col('ECON3').startswith("4807")
+                     | col('ECON3').startswith("4808")
+                     | col('ECON3').startswith("4816")
+                     | col('ECON3').startswith("4819")
+                     | col('ECON3').startswith("4821")
+                     | col('ECON3').startswith("4822")
+                    ), 'basic services'
+                ).when(
+                    (col('ECON3').startswith("4849")
+                     | col('ECON3').startswith("4851")
+                     | col('ECON3').startswith("4874")
+                    ), 'employment contracts'
+                )
+            )
+            .when(
+                col('ECON2') == '6300-Pensions and Gratuities',
+                'pensions')
+            .when(
+                (col('ECON2').startswith('59') & (col('FUNC1') == 'Social Security and Welfare')),
+                'social assistance'
+            )
+        ).withColumn(
+            'econ',
+            when(
+                col('ECON1') == "6800-Capital Expenditure",
+                'Capital expenditures')
+            .when(
+                col('econ_sub').isin([
+                    'basic wages',
+                    'allowances']),
+                'Wage bill')
+            .when(
+                col('ECON2').isin([
+                    "4800-Supplies and Services",
+                    "4900-Repairs and Maintenance"]),
+                'Goods and services')
+            .when(
+                col('ECON2').isin([
+                    "5000-Term Loan Interest",
+                    "5100-Floating Loan Interest",
+                    "5200-Interest on National Savings Certificates",
+                    "5300-Provident Fund Interest",
+                    "5400-Postal Life Insurance Interest",
+                    "5500-Other Interest",
+                    "5600-Interest on Foreign Debt"
+                ]),
+                'Interest on debt'
+            ) 
+            .when(
+                col('ECON2').isin([
+                    "6400-State Trading",
+                    "5800-Subsidies and Incentives"]),
+                'Subsidies')
+            .when(
+                col('econ_sub').isin([
+                    'social assistance',
+                    'pensions']),
+                'Social benefits')
+            .otherwise('Other expenses') 
         )
     )
+
+
+# COMMAND ----------
+
+@dlt.table(name=f'bgd_boost_gold')
+def boost_gold():
+    gold_cols = [
+        'year',
+        col('BUDGET').alias('approved'),
+        col('REVISED').alias('revised'),
+        col('EXECUTED').alias('executed'),
+        'admin0',
+        'admin1',
+        'admin2',
+        'geo1',
+        'func',
+        'func_sub',
+        'econ',
+        'econ_sub',
+    ]
+
+    return (
+        dlt.read(f'bgd_2019_onward_boost_silver')
+        .select(*gold_cols)
+        .union(
+            dlt.read(f'bgd_2015_to_2018_boost_silver')
+            .select(*gold_cols)
+        )
+        .withColumn('country_name', lit(COUNTRY))
+    )
+
