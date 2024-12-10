2e3cfd1150705c813a6e206f2173a9c06e173684
bhupatiraju <bhupatiraju>
2024-12-10 17:45:35 +0000

    Modifications to take into account eh discrepancies. Added name mapping to align with subnational population.

--- Chile/CHL_transform_load_dlt.py
+++ Chile/CHL_transform_load_dlt.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 import dlt
 import unicodedata
-from pyspark.sql.functions import substring, col, lit, when, udf, trim, regexp_replace, initcap, concat, lower
+from pyspark.sql.functions import substring, col, lit, when, udf, trim, regexp_replace, initcap, concat, lower, create_map
 from pyspark.sql.types import StringType
 
 # Note DLT requires the path to not start with /dbfs
@@ -18,6 +18,26 @@ CSV_READ_OPTIONS = {
     "escape": '"',
 }
 
+region_mapping = {
+    "IX": "Araucanía",
+    "RM": "Región Metropolitana de Santiago",
+    "I": "Tarapacá",
+    "II": "Antofagasta",
+    "III": "Atacama",
+    "IV": "Coquimbo",
+    "V": "Valparaíso",
+    "VI": "Libertador General Bernardo O'Higgins",
+    "VII": "Maule",
+    "VIII": "Biobío",
+    "X": "Los Lagos",
+    "XI": "Aysén",
+    "XII": "Magallanes y la Antártica Chilena",
+    "XIV": "Los Ríos",
+    "XV": "Arica y Parinacota",
+    "XVI": "Ñuble",
+}
+region_mapping_expr = create_map([lit(key) for pair in region_mapping.items() for key in pair])
+
 @dlt.expect_or_drop("year_not_null", "year IS NOT NULL")
 @dlt.table(name=f'chl_boost_bronze_cen')
 def boost_bronze_cen():
@@ -27,12 +47,32 @@ def boost_bronze_cen():
             .options(**CSV_READ_OPTIONS)
             .option("inferSchema", "true")
             .load(f'{COUNTRY_MICRODATA_DIR}/Cen.csv')
+            .withColumn("region", when(col("region").isNull(), "").otherwise(col("region").cast("string")))
             .withColumn("econ1", col("econ1").cast("string"))
             .withColumn("transfer", col("transfer").cast("string"))
             .withColumn("econ2", col("econ2").cast("string"))
+            .withColumn("region", region_mapping_expr.getItem(col("region")))
             .filter(
-                ((col('year')<2017) & (lower(col('transfer'))=='excluye consolidables') & (~col('econ2').rlike('^(28|32|34|30)'))) |
-                ((col('year')>2016) & (~(lower(col('econ1')).isin('financiamiento', 'consolidable'))))
+                (
+                    (col('year') < 2017) & 
+                    ((lower(col('transfer')) == 'excluye consolidables') & (~col('econ2').rlike('^(32|30)'))) &
+                    (
+                        (~col('econ2').rlike('^(28|34)')) | 
+                        (lower(col('econ3')).isin("03 intereses deuda interna",
+                                           "04 intereses deuda externa", 
+                                           "05 otros gastos financieros deuda interna", 
+                                           "06 otros gastos financieros deuda externa"))
+                    )
+                ) |
+                ((col('year') > 2016) & 
+                 (
+                    (~(lower(col('econ1')).isin('financiamiento', 'consolidable'))) | 
+                    (lower(col('econ3')).isin('04 intereses deuda externa', 
+                                       '06 otros gastos financieros deuda externa',
+                                       '03 intereses deuda interna',
+                                       '05 otros gastos financieros deuda interna'))
+                    )
+                 )
             )
     )
 
@@ -49,8 +89,11 @@ def boost_bronze_municipal():
             .withColumn("econ2", when(col("econ2").isNull(), "").otherwise(col("econ2").cast("string")))
             .withColumn("econ3", when(col("econ3").isNull(), "").otherwise(col("econ3").cast("string")))
             .withColumn("econ4", when(col("econ4").isNull(), "").otherwise(col("econ4").cast("string")))
+            .withColumn("Region", when(col("Region").isNull(), "").otherwise(col("Region").cast("string")))
            .filter((col('econ0') == '2 Gasto') & (lower(col('econ1')) != 'gastos por actividades de financiacion'))
            .withColumnRenamed('accrued', 'executed')
+           .withColumnRenamed('Servicio', 'service')
+           .withColumnRenamed('Region', 'region')
     )
     df2 = (spark.read
            .format("csv")
@@ -62,11 +105,14 @@ def boost_bronze_municipal():
             .withColumn("ECON2", when(col("ECON2").isNull(), "").otherwise(col("ECON2").cast("string")))
             .withColumn("ECON3", when(col("ECON3").isNull(), "").otherwise(col("ECON3").cast("string")))
             .withColumn("ECON4", when(col("ECON4").isNull(), "").otherwise(col("ECON4").cast("string")))
+            .withColumn("Region", when(col("Region").isNull(), "").otherwise(col("Region").cast("string")))
            .filter((col('ECON0') == '2 Gasto') & (lower(col('ECON1')) != 'gastos por actividades de financiacion'))
            .withColumnRenamed('ACCRUED', 'executed')
+           .withColumnRenamed('Service2', 'service')
+           .withColumnRenamed('Region', 'region')
     )
     df2 = df2.toDF(*[col.lower() for col in df2.columns])
-    df = df1.unionByName(df2, allowMissingColumns=True)
+    df = df1.unionByName(df2, allowMissingColumns=True).withColumn("region", region_mapping_expr.getItem(col("region")))
     return df
 
 @dlt.table(name='chl_boost_silver')
@@ -78,11 +124,19 @@ def boost_silver():
         ).withColumn(
             'admin0_tmp', lit('Central')
         ).withColumn(
-            'admin1_tmp', lit('Central Scope')
-        ).withColumn('geo1', lit('')
+            'admin1_tmp', when((col('region').isNull()) | (col('region') == ''), lit('Central Scope')).otherwise(col('region'))
+        ).withColumn(
+            'admin2_tmp', col('admin2')
+        ).withColumn('geo1', lit('Central Scope')
+        ).withColumn('interest_tmp', 
+            when(lower(col('econ3')).isin('04 intereses deuda externa',
+                                          '06 otros gastos financieros deuda externa',
+                                          '03 intereses deuda interna',
+                                          '05 otros gastos financieros deuda interna'), 1)
+            .otherwise(0)
         ).withColumn('func_sub',
-            when(col('admin1').rlike('^(03|10)'), 'judiciary')
-            .when(col('admin1').startswith('05'), 'public safety')
+            when((col('admin1').rlike('^(03|10)')) & (col('interest_tmp')==0), 'judiciary')
+            .when((col('admin1').startswith('05')) & (col('interest_tmp')==0), 'public safety')
             # No breakdown of education spending into primary, secondary etc
             # No breakdown of health expenditure into primary, secondary etc
             # agriculture
@@ -103,7 +157,7 @@ def boost_silver():
         ).withColumn('func',
             when(col('admin1').startswith('11'), 'Defence')
             .when(col('func_sub').isin('judiciary', 'public safety'), "Public order and safety")
-            .when((
+            .when((col('interest_tmp')==0) & ( 
                 ((col('admin1').rlike('^(07|17)') & (~lower(col('admin2')).isin("04 comision chilena de energia nuclear", "05 comision nacional de energia")))) |
                 col('func_sub').isin('agriculture', 'road', 'railroads', 'water transport', 'air transport', 'energy')             
                 ), 'Economic affairs')
@@ -118,8 +172,8 @@ def boost_silver():
                                    '01 Primer Tribunal Ambiental')
             ), 'Environmental protection')
             .when(col('admin1').startswith('18'), 'Housing and community amenities')
-            .when(col('admin1').startswith('16'), 'Health')
-            .when(col('admin1').startswith('09'), 'Education')
+            .when((col('admin1').startswith('16')) & (col('interest_tmp')==0), 'Health')
+            .when(col('admin1').startswith('09') & (col('interest_tmp')==0), 'Education')
             # No recreation and culture spending information
             # No infromation on social protection
             .otherwise('General public services')
@@ -132,15 +186,19 @@ def boost_silver():
             # wage bill
             when(col('econ2').startswith('21'), 'Wage bill')
             # capex
-            .when(col('econ1')=='Gasto De Capital', 'Capital expenditures')
+            .when(lower(col('econ1'))=='gasto de capital', 'Capital expenditures')
             # goods and services
             .when(col('econ2').startswith('22'), 'Goods and services')
             # subsidies
-            .when(col('subsidies')=='y', 'Subsidies')
+            .when((col('subsidies')=='y') & 
+                  (lower(col('econ3')) !="02 prestaciones de asistencia social") &
+                  (lower(col('econ3')) !="01 prestaciones previsionales") &
+                  (lower(col('econ3')) != "03 a otras entidades publicas") &
+                  (lower(col('econ1')) !="gasto de capital"), 'Subsidies')
             # social benefits
             .when(col('econ_sub').isin('social assistance', 'pensions'), 'Social benefits')
             # Other grants and trasnfers
-            .when(lower(col('econ3'))=='03 a otras entidades publicas', 'Other grants and transfers')
+            .when((lower(col('econ3'))=='03 a otras entidades publicas') & (lower(col('econ1'))!='gasto de capital'), 'Other grants and transfers')
             # interest on debt
             .when(lower(col('econ3')).isin("03 intereses deuda interna",
                                             "04 intereses deuda externa",
@@ -154,10 +212,14 @@ def boost_silver():
             'sheet', lit('Mun')
         ).withColumn(
             'admin0_tmp', lit('Regional')
-        ).withColumn('geo1', lit('')
+        ).withColumn(
+            'admin1_tmp', col('Region')
+        ).withColumn(
+            'admin2_tmp', col('Municipio')
+        ).withColumn('geo1', col('Region')
         ).withColumn('func',
-            when(lower(col('service2'))=='area de salud', 'Health')
-            .when(trim(lower(col('service2')))=='area de educacion', 'Education')
+            when(lower(col('service'))=='area de salud', 'Health')
+            .when(trim(lower(col('service')))=='area de educacion', 'Education')
             .otherwise('General public services')
         ).withColumn('econ',
             # wage bill
@@ -178,6 +240,7 @@ def boost_silver():
 def boost_gold():
     return (dlt.read(f'chl_boost_silver')
         .withColumn('country_name', lit(COUNTRY))
+        #.filter(col('year') > 2008)
         .select('country_name',
                 col('year').cast("integer"),
                 'approved',
