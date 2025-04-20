8b7d2d5807faa05e38654c6baa5ef4c2540e65c0
elysenko <elysenko>
2025-04-20 01:19:25 +0000

    Applying End Year filter

--- quality/transform_load_dlt.py
+++ quality/transform_load_dlt.py
@@ -15,6 +15,8 @@ CSV_READ_OPTIONS = {
     "escape": '"',
 }
 
+end_year = 2024
+
 # COMMAND ----------
 
 @dlt.table(name=f'quality_cci_bronze')
@@ -51,7 +53,7 @@ def quality_total_silver():
             valueColumnName="amount"
         )
         .filter(F.col('amount').isNotNull())
-        .filter(F.col('year') < 2024)
+        .filter(F.col('year') < end_year)
     )
 
 # COMMAND ----------
@@ -107,7 +109,7 @@ def quality_functional_silver():
             valueColumnName="amount"
         )
         .filter(F.col('amount').isNotNull())
-        .filter(F.col('year') < 2024)
+        .filter(F.col('year') < end_year)
     )
 
 # COMMAND ----------
@@ -158,7 +160,7 @@ def quality_economic_silver():
             valueColumnName="amount"
         )
         .filter(F.col('amount').isNotNull())
-        .filter(F.col('year') < 2024)
+        .filter(F.col('year') < end_year)
     )
 
 # COMMAND ----------
@@ -189,7 +191,7 @@ def quality_judiciary_silver():
             valueColumnName="amount"
         )
         .filter(F.col('amount').isNotNull())
-        .filter(F.col('year') < 2024)
+        .filter(F.col('year') < end_year)
     )
 
 # COMMAND ----------
@@ -222,7 +224,7 @@ def quality_total_subnat_silver():
             valueColumnName="amount"
         )
         .filter(F.col('amount').isNotNull())
-        .filter(F.col('year') < 2024)
+        .filter(F.col('year') < end_year)
     )
 
 # COMMAND ----------
@@ -254,7 +256,7 @@ def quality_total_foreign_silver():
             valueColumnName="amount"
         )
         .filter(F.col('amount').isNotNull())
-        .filter(F.col('year') < 2024)
+        .filter(F.col('year') < end_year)
     )
 
 # COMMAND ----------
